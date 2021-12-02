94828e3d7201ae2ce43a6d0b3278f2c40fd771bd
Mrbarnk <mrbarnk1@gmail.com>
2021-12-02 18:09:58 +0100

    Utils update: Added response data

--- src/utils/responses.utils.ts
+++ src/utils/responses.utils.ts
@@ -1,8 +1,8 @@
 import { Response } from "express";
 
-export const successResponse = (res: Response, message: String, statusCode: number = 200) => {
-    return res.status(statusCode).json({status: true, message})
+export const successResponse = (res: Response, message: String, statusCode: number = 200, data: any = []) => {
+    return res.status(statusCode).json({ status: true, message, data })
 }
-export const errorResponse = (res: Response, message: String = 'Something went wrong', statusCode: number = 400) => {
-    return res.status(statusCode).json({status: false, message})
+export const errorResponse = (res: Response, message: String = 'Something went wrong', statusCode: number = 500, error: any = {}) => {
+    return res.status(statusCode).json({ status: false, message, error })
 }
